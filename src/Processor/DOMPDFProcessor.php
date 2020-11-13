b13d767c95cf1417779dca847db1d5f9eefb09b8
Nur Hidayatullah <kematjaya0@gmail.com>
2020-11-13 17:10:49 +0700

    update pdf return response

--- src/Processor/DOMPDFProcessor.php
+++ src/Processor/DOMPDFProcessor.php
@@ -4,6 +4,8 @@ namespace Kematjaya\Export\Processor;
 
 use Kematjaya\Export\Processor\PDFProcessor;
 use Dompdf\Dompdf;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Nur Hidayatullah <kematjaya0@gmail.com>
@@ -21,11 +23,14 @@ class DOMPDFProcessor extends PDFProcessor
         $pdf = new Dompdf();
         $pdf->loadHtml($content);
         $pdf->render();
-        // TODO: header already sent
         
-        return $pdf->stream($this->getFileName(), [
-            "Attachment" => $viewMode === self::ATTACHMENT_VIEW ? false : true
-        ]);
+        $disposition = $viewMode === self::ATTACHMENT_VIEW ? ResponseHeaderBag::DISPOSITION_INLINE : ResponseHeaderBag::DISPOSITION_ATTACHMENT;
+        
+        $response = new Response($pdf->output());
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', $disposition);
+        
+        return $response;
     }
 
 }
