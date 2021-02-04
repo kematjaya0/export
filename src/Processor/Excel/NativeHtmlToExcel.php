8a4b31880d915488c10efa7f34a601ffd46d7360
Nur Hidayatullah <kematjaya0@gmail.com>
2021-02-05 06:54:21 +0700

    create NativeHtmlToExcel

---
+++ src/Processor/Excel/NativeHtmlToExcel.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of the export.
+ */
+
+namespace Kematjaya\Export\Processor\Excel;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+
+/**
+ * @package Kematjaya\Export\Processor\Excel
+ * @license https://opensource.org/licenses/MIT MIT
+ * @author  Nur Hidayatullah <kematjaya0@gmail.com>
+ */
+class NativeHtmlToExcel extends ExcelProcessor
+{
+    
+    public function isSupported($data): bool 
+    {
+        return is_string($data);
+    }
+
+    public function render($data, string $viewMode) 
+    {
+        $crawler = new Crawler($data);
+        $excel = sprintf('<table border="1">%s</table>', $crawler->filter('table')->html());
+        
+        return $this->createResponse($excel, $viewMode);
+    }
+
+    protected function createResponse(string $content, string $viewMode = self::ATTACHMENT_VIEW): Response
+    {
+        $response = new Response($content);
+        $response->headers->set('Content-type', 'application/vnd-ms-excel');
+        $response->headers->set('Content-Disposition', HeaderUtils::makeDisposition($viewMode, $this->getFileName()));
+        return $response;
+    }
+}
